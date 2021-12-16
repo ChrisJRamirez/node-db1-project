d692b04238a5ca0422348f0b1c9b3d9f08798220
ChrisJRamirez <chrisjamesramirez@gmail.com>
2021-12-15 22:29:23 -0500

    had all tests passing, then broke code, now 23/24 tests passing but app still crashing

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,16 +1,43 @@
+const db = require("..//..//data/db-config");
 const Account = require("../accounts/accounts-model")
 
 exports.checkAccountPayload = async (req, res, next) => {
     const {name, budget} = req.body
-    if(name && budget){
-      next();
-    }else{
-      res.status(400).json({message:"name and budget are required"})
-    } 
-}
+    if(name === undefined || budget ===undefined){
+      res.status(400).json({message:"name and budget are required"}) 
+      } 
+      else if (typeof name !=="string"){
+      res.status(400).json({message:"name of account must be a string"}) 
+      } 
+      else if (name.trim().length < 3 || name.trim() > 100){
+      res.status(400).json({message:"name of account must be between 3 and 100"}) 
+      } 
+      else if (typeof budget !== "number" || isNaN(budget)){
+      res.status(400).json({message:"budget of account must be a number"}) 
+      } 
+      else if(budget <0 || budget > 1000000){
+      res.status(400).json({message:"budget of account is too large or too small"}) 
+      next()
+    }
+     else {
+      next()
+    }
+  }
 
-exports.checkAccountNameUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+exports.checkAccountNameUnique = async (req, res, next) => {
+  try{
+    const existingName = await db("accounts")
+    .where("name", req.body.name.trim())
+    .first()
+
+    if(existingName){
+      next ({ status: 400, message: "that name is taken"})
+    } else {
+      next()
+    }
+  } catch(err) {
+    next(err)
+  }
 }
 
 exports.checkAccountId =async (req, res, next) => {
--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -6,8 +6,8 @@ const router = require('express').Router();
 
 router.get('/', async (req, res, next) => {
   try{
-    const data = await Account.getAll()
-      res.json(data)
+    const accounts = await Account.getAll()
+      res.json(accounts)
   }catch(err){
     next(err)
   }
@@ -15,46 +15,41 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', checkAccountId, async (req, res, next) => {
   try{
-    const data = await Account.getById(req.params.id);
-    res.json(data)
-
+    const account = await Account.getById(req.params.id);
+    res.json(account)
   }catch(err){
     next(err)
   } 
-})
+});
 
-router.post('/',checkAccountPayload , async (req, res, next) => {
+router.post('/',checkAccountPayload, checkAccountNameUnique, async (req, res, next) => {
   try{
-    const data = await Account.create(req.body);
-    res.json(data)
+    const newAccount = await Account.create({
+      name: req.body.name.trim(),
+      budget: req.body.budget
+    });
+    res.status(201).json(newAccount)
   }catch(err){
     next(err)
   }
-})
+});
 
 router.put('/:id', checkAccountId, checkAccountPayload, async (req, res, next) => {
   try{
-    const data = await Account.updateById(req.params.id, req.body)
-    res.json(data)
-  }catch(err){
+    const updated = await Account.updateById(req.params.id, req.body);
+    res.status(200).json(updated) 
+  } catch (err){
     next(err)
   }
 });
 
 router.delete('/:id', checkAccountId, async (req, res, next) => {
   try{
-    const data = await Account.deleteById(req.params.id)
-    res.json(data)
+    const deletedAccount = await Account.deleteById(req.params.id)
+    res.json(deletedAccount)
   }catch(err){
     next(err)
   }
-})
-
-router.use((err, req, res, next) => { // eslint-disable-line
-  res.status(err.status || 500).json({
-    message: err.message,
-    stack: err.stack,
-  })
-})
+});
 
 module.exports = router;
